01f8d4f63d8b7df49b5306c6b15cececaf1520ff
bxvail <BryantVail@gmail.com>
2018-03-26 01:32:14 -0400

    merging in stripped project w/outJSX
    reading all orders, 'orders.html' works

--- server.js
+++ server.js
@@ -1,82 +1,145 @@
-const express       = require('express');
-const bodyParser    = require('body-parser');
-const MongoClient   = require('mongodb').MongoClient;
+/* global __dirname,  */
+/* eslint-disable no-alert, no-console */
 
-const morgan        = require('morgan');
-const path          = require('path');
-
+const express       = require("express");
+const path          = require("path");
+const fs            = require("fs");
+const bodyParser    = require("body-parser");
+const MongoClient   = require("mongodb").MongoClient;
+const morgan        = require("morgan");
+const React         = require("react");
 
 const app = express();
 
-app.set('json spaces', 4);
+app.set("json spaces", 4);
+//let publicPath = path.resolve(__dirname, "static");
 
+
+
+//app.use(express.static(publicPath));
 app.use(morgan("default"));
-
-var publicPath = path.resolve(__dirname, "static");
-app.use(express.static(publicPath));
 app.use(bodyParser.json());
-
-
-
-app.get('/',(req,res)=>{
-    res.sendFile("orders.html");
-    console.log(req.body.json());
+app.use(function(req, res, next){
+  console.log("RequestIP: \t"+ req.ip);
+  console.log("Request Date: \t"+ new Date());
+  next();
 });
 
-app.get('/api/orders', (req,res) => {
-    
-    db.collection('orders').find().toArray().then(orders =>{
-        
-        const metadata = {total_count:orders.length};
-        //test console
-        console.log('processed "/api/orders"'+orders.forEach(
-            function(element){            
-                console.log(element._id+"\n");
-                }
-            )+console.log(metadata.total_count)
-        );//end test console
-        
-        res.json({_metadata: metadata, records:orders});
-    }).catch(error =>{
-        console.log(error);
-        res.status(500).json({message:'Internal Server Error: ${error}'});
-    });
-});
-
-app.post('/api/orders', (req,res) => {
-    const newOrder = req.body;
-
-    
-    newOrder.dateCreated = new Date();
-
-    if(!newOrder.status){
-        newOrder.status = "New";
+app.use(function(req, res, next){
+  var filePath = path.join(__dirname, "static", req.url);
+  fs.stat(filePath, function(err, fileInfo){
+    if(err){
+      next();
+      return;
     }
-    const err = Order.validateOrder(NewOrder)
-        if(err){
-            res.status(422).json({message:'invalid request: ${err}'});
-            return;
-        }
-    
-    db.collection('orders').insertOne(newOrder).then(result =>
-        db.collection('orders').find({_id: result.insertedId}).limit(1).next()
-    ).then(newOrder =>{
-        res.json(newOrder);
-    }).catch(error => {
-        console.log(error);
-        res.status(500).json({message:'Internal Server Error: ${error}'});
-    });
+    if(fileInfo.isFile()){
+      res.sendFile(filePath);
+    }else{
+      next();
+    }
+  });
+});
+
+
+app.get("/",(req,res)=>{
+  res.redirect("orders.html");
+  console.log(req.body.json());
+
+});
+
+app.get("/api/orders", (req,res) => {
+  db.collection("orders").find().toArray().then(orders =>{
+        
+    const metadata = {total_count:orders.length};
+    //test console
+    console.log(`processed "/api/orders" \n${orders.forEach(
+      function(element){            
+        console.log(element._id+"\n");
+      }
+    )} \n total count:\t${metadata.total_count}`
+
+    );//end test console
+        
+    res.json({_metadata: metadata, records:orders, React: React});
+  }).catch(error =>{
+    console.log(error);
+    res.status(500).json({message:`Internal Server Error: ${error}`});
+  });
+});
+
+const validOrderStatus = {
+  New: true,
+  UnderReview: true, 
+  ActionNeeded: true,
+  Quoted: true, 
+  PaymentNeeded:true,
+  Processing: true, 
+  Shipped: true, 
+  Complete: true, 
+  Cancelled: true
+};
+
+const orderFieldType = {
+  items: "optional", 
+  orderDescription: "required", 
+  userID: "required", 
+  needByDate: "optional",
+  orderTitle: "required"
+};
+
+function validateOrder(order){
+  for(const field in orderFieldType){
+    let type = orderFieldType[field];
+    if(!type){
+      delete order[field];
+    } else if(type === "required" && !order[field]){
+      return `${field} is required.`;
+    }
+  }//end for
+
+  if(!validOrderStatus[order.status]){
+    return `${order.status} is not a valid status.`;
+  }
+  return null;
+
+}//end validateOrder
+
+
+
+app.post("/api/orders", (req,res) => {
+  const newOrder = req.body;
+  newOrder.dateCreated = new Date();
+
+  if(!newOrder.status){
+    newOrder.status = "New";
+  }
+  const err = validateOrder(newOrder);
+  if(err){
+    return res.status(422).json({message:`invalid request: ${err}`});
+  }
+     
+  db.collection("orders").insertOne(newOrder).then(result =>
+    db.collection("orders").find({_id: result.insertedId}).limit(1).next()
+  ).then(newOrder =>{
+    res.json(newOrder);
+  }).catch(error => {
+    console.log(error);
+    res.status(500).json({message:`Internal Server Error: ${error}`});
+  });
 });
 
 
 let db;
-MongoClient.connect('mongodb://localhost/OrderTracker').then(connection => {
-    db = connection;
-    app.listen(3000, () =>{
-        console.log('App status:Running, port:3000');
-    });
+let port = 1990;
+let mongoPath = "mongodb://localhost/OrderTracker";
+
+MongoClient.connect(`${mongoPath}`).then(connection => {
+  db = connection;
+  app.listen(1990, () =>{
+    console.log(`App status:Running, port:${port}`);
+  });
 }).catch(error =>{
-    console.log('ERROR:', error);
+  console.log(`ERROR: ${error}`);
 });
 
 
--- static/orders.js
+++ static/orders.js
@@ -1,292 +1,246 @@
-'use strict';
-var _OrderAdd = require('./OrderAdd.js');
-
-var _createClass = function () { 
-    function defineProperties(target, props){ 
-      for (var i = 0; i < props.length; i++) {
-        var descriptor = props[i]; 
-        descriptor.enumerable = descriptor.enumerable || false; 
-        descriptor.configurable = true; 
-        if ("value" in descriptor){
-          descriptor.writable = true; 
-        }        
-        Object.defineProperty(target, descriptor.key, descriptor); 
-      } 
-    } 
-  
-  //enhancing object props
-  return function (Constructor, protoProps, staticProps) {
-    if (protoProps) {
-      defineProperties(Constructor.prototype, protoProps);
-    }      
-    if (staticProps) {
-      defineProperties(Constructor, staticProps);
-    }
-    return Constructor; 
-  }; 
-}();
+"use strict";
+/* eslint-disable no-alert, no-console */
+/* global React, ReactDOM, props*/
 
 
+let _OrderAdd = require("./OrderAdd.js");
+let contentNode = document.getElementById("contents");
 
-var _OrderAdd2 = _interopRequireDefault(_OrderAdd);
-
-function _interopRequireDefault(obj) { 
-    return obj && obj.__esModule ? obj : { default: obj }; 
-}
-
-function _classCallCheck(instance, Constructor) { 
-  if (!(instance instanceof Constructor)) {
-     throw new TypeError("Cannot call a class as a function"); 
-    } 
-  }
-
-function _possibleConstructorReturn(self, call) {
-   if (!self) {
-      throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); 
-    } 
-    return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-var contentNode = document.getElementById('contents');
-
-var OrderFilter = function (_React$Component) {
-  _inherits(OrderFilter, _React$Component);
-
-  function OrderFilter() {
-    _classCallCheck(this, OrderFilter);
-
-    return _possibleConstructorReturn(this, (OrderFilter.__proto__ || Object.getPrototypeOf(OrderFilter)).apply(this, arguments));
-  }
-
-  _createClass(OrderFilter, [{
-    key: 'render',
-    value: function render() {
-      return React.createElement(
-        'div',
-        null,
-        'This is a placeholder for the Order Filter.'
-      );
-    }
-  }]);
-
-  return OrderFilter;
-}(React.Component);
-
-var OrderRow = function OrderRow(props) {
-  return React.createElement(
-    'tr',
-    null,
-    React.createElement(
-      'td',
-      null,
-      props.order._id
-    ),
-    React.createElement(
-      'td',
-      null,
-      props.order.status
-    ),
-    React.createElement(
-      'td',
-      null,
-      props.order.owner
-    ),
-    React.createElement(
-      'td',
-      null,
-      props.order.created.toDateString()
-    ),
-    React.createElement(
-      'td',
-      null,
-      props.order.effort
-    ),
-    React.createElement(
-      'td',
-      null,
-      props.order.effort
-    ),
-    React.createElement(
-      'td',
-      null,
-      props.order.completionDate ? props.issue.completionDate.toDateString() : ''
-    ),
-    React.createElement(
-      'td',
-      null,
-      props.order.title
-    )
+let OrderFilter = Object.assign(OrderFilter, React.Component.prototype);
+OrderFilter.render = function(){
+  return(
+    React.createElement("div", null, "This is a Placeholder for Order Filter")
   );
 };
 
-function OrderTable(props) {
-  var orderRows = props.orders.map(function (issue) {
-    return React.createElement(OrderRow, { key: order._id, order: order });
+/*
+*   Constructor re-defining only needed when there may be 
+*     ->other initiation statements necessary
+*/
+
+//OrderRow{}
+let OrderRow = Object.assign(OrderRow, React.Component.prototype);
+
+//constructor()
+Object.defineProperty(OrderRow, 
+  OrderRow.prototype.constructor, 
+  function(props){
+    this.props = props;
   });
-  return React.createElement(
-    'table',
-    { className: 'bordered-table' },
-    React.createElement(
-      'thead',
-      null,
+
+//render()    
+Object.defineProperty(OrderRow, 
+  OrderRow.prototype.render, 
+  function(){
+    return (
       React.createElement(
-        'tr',
+        "tr",
         null,
         React.createElement(
-          'th',
+          "td",
           null,
-          'Id'
+          props.order.user
         ),
         React.createElement(
-          'th',
+          "td",
           null,
-          'Status'
+          props.order.status
         ),
         React.createElement(
-          'th',
+          "td",
           null,
-          'Owner'
+          props.order.owner
         ),
         React.createElement(
-          'th',
+          "td",
           null,
-          'Created'
+          props.order.dateCreated.toDateString()
         ),
         React.createElement(
-          'th',
+          "td",
           null,
-          'Effort'
+          props.order.effort
         ),
         React.createElement(
-          'th',
+          "td",
           null,
-          'Completion Date'
+          props.order.dateCompleted ? props.order.dateCompleted.toDateString() : "Open"
         ),
         React.createElement(
-          'th',
+          "td",
           null,
-          'Title'
-        )
-      )
-    ),
-    React.createElement(
-      'tbody',
-      null,
-      orderRows
-    )
-  );
-}
+          props.order.title
+        )//end last tableData
+      )//close tableRow/tr
+    );//end reder() return statement
+  });//end function OrderRow.render
+//end OrderRow{}    
 
-var OrderList = function (_React$Component2) {
-  _inherits(OrderList, _React$Component2);
+//OrderTable{}
+let OrderTable= Object.assign({}, React.Component.prototype);
 
-  function OrderList() {
-    _classCallCheck(this, OrderList);
-
-    var _this2 = _possibleConstructorReturn(this, (OrderList.__proto__ || Object.getPrototypeOf(OrderList)).call(this));
-
-    _this2.state = { orders: [] };
-
-    _this2.createOrder = _this2.createOrder.bind(_this2);
-    return _this2;
+//constructor(), init orderRows
+Object.defineProperty(OrderTable, 
+  OrderTable.prototype.constructor, 
+  function(props){
+    this.props = props;
+    this.orderRows = props.orders.map(function (order) {
+      return React.createElement(OrderRow, { key: order._id, order: order });
+    });
   }
+);//end OrderTable.prototype.constructor
 
-  _createClass(OrderList, [{
-    key: 'componentDidMount',
-    value: function componentDidMount() {
-      this.loadData();
-    }
-  }, {
-    key: 'loadData',
-    value: function loadData() {
-      var _this3 = this;
-
-      var myInit = {
-        method: 'GET',
-        mode: 'no-cors',
-        credentials: 'omit',
-        origin: 'http://localhost:3000/'
-      };
-
-      fetch('http://localhost:3000/api/orders', myInit).then(function (response) {
-        if (response.body.records) {
-          response.body.json().then(function (data) {
-            console.log("Total count of records:", data._metadata.total_count);
-            data.records.forEach(function (order) {
-              order.created = new Date(order.created);
-              if (order.completionDate) order.completionDate = new Date(order.completionDate);
-            });
-            _this3.setState({ orders: data.records });
-          });
-        } else {
-          response.json().then(function (error) {
-            alert("Failed to fetch orders:" + error.message);
-          });
-        }
-      }).catch(function (err) {
-        alert("Error in fetching data from server:" + err.message, err.message);
-      });
-    }
-
-    /*
-      var xhr = new XMLHttpRequest();
-      var method  = 'GET';
-      var url     = 'http://localhost:3000/api/orders';
-      xhr.open(method, url);
-    */
-
-  }, {
-    key: 'createOrder',
-    value: function createOrder(newOrder) {
-      var _this4 = this;
-
-      fetch('/api/orders', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(newOrder)
-      }).then(function (response) {
-        if (response.ok) {
-          response.json().then(function (updatedOrder) {
-            updatedOrder.created = new Date(updatedOrder.created);
-            if (updatedOrder.completionDate) updatedOrder.completionDate = new Date(updatedOrder.completionDate);
-            var newOrders = _this4.state.orders.concat(updatedOrder);
-            _this4.setState({ orders: newOrders });
-          });
-        } else {
-          response.json().then(function (error) {
-            alert("Failed to add order: " + error.message);
-          });
-        }
-      }).catch(function (err) {
-        alert("Error in sending data to server: " + err.message);
-      });
-    }
-  }, {
-    key: 'render',
-    value: function render() {
-      return React.createElement(
-        'div',
+//render()
+Object.defineProperty(OrderTable, 
+  OrderTable.prototype.render, 
+  function(){
+    React.createElement(
+      "table",
+      { className: "bordered-table" },
+      React.createElement(
+        "thead",
         null,
         React.createElement(
-          'h1',
+          "tr",
           null,
-          'Order Tracker'
-        ),
-        React.createElement(OrderFilter, null),
-        React.createElement('hr', null),
-        React.createElement(OrderTable, { orders: this.state.orders }),
-        React.createElement('hr', null),
-        React.createElement(_OrderAdd2.default, { createOrder: this.createOrder })
-      );
-    }
-  }]);
+          React.createElement(
+            "th",
+            null,
+            "User"
+          ),
+          React.createElement(
+            "th",
+            null,
+            "Status"
+          ),
+          React.createElement(
+            "th",
+            null,
+            "Owner"
+          ),
+          React.createElement(
+            "th",
+            null,
+            "Created"
+          ),
+          React.createElement(
+            "th",
+            null,
+            "Completion Date"
+          ),
+          React.createElement(
+            "th",
+            null,
+            "Title"
+          )
+        )
+      ),
+      React.createElement(
+        "tbody",
+        null,
+        this.orderRows
+      )
+    );
+  }
+);//end render()
+//end OrderTable{}
+  
+//OrderList{}
+let OrderList = Object.assign({}, React.Component.prototype);
 
-  return OrderList;
-}(React.Component);
+//componentDidMount()
+Object.defineProperty(OrderList, 
+  OrderList.prototype.componentDidMount,
+  function(){
+    this.loadData();
+  });
 
-ReactDOM.render(React.createElement(OrderList, null), contentNode); // Render the component inside the content Node
 
-module.exports = {
-  orders: orders
-};
+//loadData()    
+Object.defineProperty(OrderList, 
+  OrderList.prototype.loadData, 
+  function(){
+
+    //init for fetch(url, initObj)
+    let myInit = {
+      method: "GET",
+      mode: "no-cors",
+      credentials: "omit",
+      origin: "http://localhost:1990/"
+    };
+
+    fetch("http://localhost:1990/api/orders", myInit).then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          console.log("Total count of records:", data._metadata.total_count);
+          data.records.forEach((order) => {
+            order.dateCreated = new Date(order.dateCreated);
+            if (order.dateCompleted) {
+              order.dateCompleted = new Date(order.dateCompleted);
+            }
+                        
+          });
+          //setState
+          this.setState({ orders: data.records });
+        });//end response.json
+      } else {
+        response.json().then(function (error) {
+          alert("Failed to fetch orders:" + error.message);
+        });
+      }
+    }).catch(function (err) {//catch for fetch("http://localhost:1990/api/orders", myInit).then(function (response) {
+      alert("Error in fetching data from server:" + err.message, err.message);
+    });
+  });//end loadData
+
+//createOrder()
+Object.defineProperty(OrderList, 
+  OrderList.prototype.createOrder, 
+  function(newOrder){
+    fetch("/api/orders", 
+      {
+        //init parameter
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(newOrder)
+      }
+    ).then(function (response) {
+      if (response.ok) {
+        response.json().then(function (updatedOrder) {//in JSON
+          updatedOrder.created = new Date(updatedOrder.created);
+          if (updatedOrder.completionDate) {
+            updatedOrder.completionDate = new Date(updatedOrder.completionDate);
+          }
+          //if updatedOrder.completionDate is true, then create a new date w/it & update the view 
+          // -> to show the successfully persisted new record
+          var newOrders = this.state.orders.concat(updatedOrder);
+          this.setState({ orders: newOrders });
+        });//end response.json
+      } else {
+        //if else, we know its an error
+        response.json().then(function (error) {
+          alert("Failed to add order: " + error.message);
+        });
+      }
+    }).catch(function (err) {
+      alert("Error in sending data to server: " + err.message);
+    });
+  });//end createOrder
+
+//render()
+Object.defineProperty(OrderList, 
+  OrderList.prototype.render, 
+  function(){
+    return React.createElement(
+      "div",
+      null,
+      React.createElement("h1",null,"OrderTracker"),
+      React.createElement(OrderFilter.render, null),
+      React.createElement("hr", null),
+      React.createElement(OrderTable.render, { orders: this.state.orders }),
+      React.createElement("hr", null),
+      React.createElement(_OrderAdd.render, { createOrder: this.createOrder })
+    );
+  });//end render
+      
+ReactDOM.render(React.createElement(OrderList.render, null), contentNode);
